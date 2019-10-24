a901a97baa012e4b45ad50d35cdc4e0bb614f1f2
allemanfredi <alle.manfredi@gmail.com>
2019-10-24 22:01:27 +0200

    Updates

---
+++ precache-manifest.9a5247b4194bfadf399a5eb83c280f2f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "937d7d8fac59404b2771",
+    "url": "/static/js/main.937d7d8f.chunk.js"
+  },
+  {
+    "revision": "cc83affe08288dab6b5c",
+    "url": "/static/js/1.cc83affe.chunk.js"
+  },
+  {
+    "revision": "71eba7c8681e9830d25f82baef75e2d2",
+    "url": "/index.html"
+  }
+];
