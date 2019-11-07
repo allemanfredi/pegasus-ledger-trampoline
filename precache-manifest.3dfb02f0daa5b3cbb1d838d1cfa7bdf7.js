ef513b8a3b1d9eaedf80763f2f5ad14766a1531e
allemanfredi <alle.manfredi@gmail.com>
2019-11-07 21:52:50 +0100

    Updates

---
+++ precache-manifest.3dfb02f0daa5b3cbb1d838d1cfa7bdf7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f38228675e7711ffb071",
+    "url": "/pegasus-ledger-trampoline/static/js/runtime~main.f3822867.js"
+  },
+  {
+    "revision": "4332e67fad0b451c27bb",
+    "url": "/pegasus-ledger-trampoline/static/js/main.4332e67f.chunk.js"
+  },
+  {
+    "revision": "b085f91f367569632415",
+    "url": "/pegasus-ledger-trampoline/static/js/1.b085f91f.chunk.js"
+  },
+  {
+    "revision": "045b866777c78b39385e154090c30ab8",
+    "url": "/pegasus-ledger-trampoline/index.html"
+  }
+];
